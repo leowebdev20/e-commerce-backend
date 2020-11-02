e05f44468ff4c7b9d6ed08b8ed915e4ccd156c64
leowebdev20 <serlebeiso@gmail.com>
2020-11-02 21:37:52 +0100

    third commit

--- routes/products.js
+++ routes/products.js
@@ -65,6 +65,7 @@ router.get('/:prodId', (req, res) => {
     .withFields(['c.title as category',
       'p.title as name',
       'p.price',
+      'p.description',
       'p.quantity',
       'p.image',
       'p.images',
@@ -110,6 +111,7 @@ router.get('/category/:catName', (req, res) => {
     .withFields(['c.title as category',
       'p.title as name',
       'p.price',
+      'p.description',   
       'p.quantity',
       'p.image',
       'p.id'
