f82c82dd14b713019daaa77fe83eb1f70317bba4
leowebdev20 <serlebeiso@gmail.com>
2020-11-17 21:01:44 +0100

    final commit

--- routes/orders.js
+++ routes/orders.js
@@ -33,9 +33,10 @@ router.get('/', (req, res) => {
         }).catch(err => res.json(err));
 });
 
-// GET SINGLE ORDER
-router.get('/:id', (req, res) => {
-  const orderId = req.params.id;
+// Get Single Order
+router.get('/:id', async (req, res) => {
+  let orderId = req.params.id;
+  console.log(orderId);
 
   database.table('orders_details as od')
       .join([
@@ -52,76 +53,87 @@ router.get('/:id', (req, res) => {
               on: 'u.id = o.user_id'
           }
       ])
-      .withFields(['o.id', 'p.title', 'p.description', 'p.price', 'u.username'])
-      .filter({'o.id': orderId})
-      .getAll()
-      .then(orders => {
-          if (orders.length > 0) {
-              res.json(orders);
-          } else {
-              res.json({message: `No orders found with id of ${orderId}!`});
-          }
+      .withFields(['o.id', 'p.title', 'p.description', 'p.price', 'p.image', 'od.quantity as quantityOrdered'])
+        .filter({'o.id': orderId})
+        .getAll()
+        .then(orders => {
+            console.log(orders);
+            if (orders.length > 0) {
+                res.json(orders);
+            } else {
+                res.json({message: "No orders found"});
+            }
 
-      }).catch(err => res.json(err));
+        }).catch(err => res.json(err));
 });
 
 // Place New Order
-router.post('/new', (req, res) => {
-  let {userId, products} = req.body;
+router.post('/new', async (req, res) => {
+    // let userId = req.body.userId;
+    // let data = JSON.parse(req.body);
+    let {userId, products} = req.body;
+    console.log(userId);
+    console.log(products);
 
-  if (userId !== null && userId > 0 && !isNaN(userId)) {
-    database.table('orders')
-      .insert({
-        user_id: userId
-      }).then(newOrderId => {
+     if (userId !== null && userId > 0) {
+        database.table('orders')
+            .insert({
+                user_id: userId
+            }).then((newOrderId) => {
 
-        if (newOrderId > 0) {
-          products.forEach(async (p) => {
-            let data = await database.table('products')
-            .filter({id: p.id})
-            .withFields(['quantity']).get();
+            if (newOrderId > 0) {
+                products.forEach(async (p) => {
 
-            let inCart = parseInt(p.incart);
+                        let data = await database.table('products').filter({id: p.id}).withFields(['quantity']).get();
 
-            // Deduct the number of pieces ordered from the quantity in database
-            if (data.quantity > 0) {
-                data.quantity = data.quantity - inCart;
-                
-                if (data.quantity < 0) {
-                data.quantity = 0;
-                }
 
+
+                    let inCart = parseInt(p.incart);
+
+                    // Deduct the number of pieces ordered from the quantity in database
+
+                    if (data.quantity > 0) {
+                        data.quantity = data.quantity - inCart;
+
+                        if (data.quantity < 0) {
+                            data.quantity = 0;
+                        }
+
+                    } else {
+                        data.quantity = 0;
+                    }
+
+                    // Insert order details w.r.t the newly created order Id
+                    database.table('orders_details')
+                        .insert({
+                            order_id: newOrderId,
+                            product_id: p.id,
+                            quantity: inCart
+                        }).then(newId => {
+                        database.table('products')
+                            .filter({id: p.id})
+                            .update({
+                                quantity: data.quantity
+                            }).then(successNum => {}).catch(err => console.log(err));
+                    }).catch(err => console.log(err));
+                });
 
             } else {
-              data.quantity = 0;
+                res.json({message: 'New order failed while adding order details', success: false});
             }
-            // Insert order details w.r.t the newly created order Id
-            database.tableList('orders_details')
-            .insert({
-              order_id: newOrderId,
-              product_id: p.id,
-              quantity: inCart
-            }).then(newId => {
-              database.table('products')
-              .filter({id: p.id})
-              .update({
-                quantity: data.quantity
-              }).then(successNum => {}).catch(err => console.log(err))
-            }).catch(err => console.log(err));
-          })
-        } else {
-          res.json({message: 'new order failed while ading order details', success: false})
-        }
-        res.json({
-          message: `Order successfully placed with Id of ${newOrderId}`,
-          success: true,
-          order_id: newOrderId,
-          products: products
-        });
-      }).catch(err => console.log(err));
-  } else {
-    res.json({message: 'new order failed', success: false});
-  }
+            res.json({
+                message: `Order successfully placed with order id ${newOrderId}`,
+                success: true,
+                order_id: newOrderId,
+                products: products
+            })
+        }).catch(err => res.json(err));
+    }
+
+    else {
+        res.json({message: 'New order failed', success: false});
+    }
+
 });
 
 // FAKE PAYMENT GATEWAY CALLS
